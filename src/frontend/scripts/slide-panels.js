eca8e0983956e3c546e55132914503ec3b3ac6ed
Igor Kozhevnikov <igor.kozhevnikov@mireon.ru>
2020-06-17 13:06:09 +0300

    Issue-1 | Run js after loaded page.

--- src/frontend/scripts/slide-panels.js
+++ src/frontend/scripts/slide-panels.js
@@ -2,7 +2,10 @@ import '@styles/slide-panels';
 import Levers from '@modules/Levers/Levers';
 import Stage from '@modules/Stage/Stage';
 
-const stage = new Stage();
-const levers = new Levers();
-levers.setToShowClickListener(lever => stage.show(lever.getTarget()));
-levers.setToHideClickListener(() => stage.hide());
+document.addEventListener('DOMContentLoaded', () => {
+    const stage = new Stage();
+    const levers = new Levers();
+
+    levers.setToShowClickListener(lever => stage.show(lever.getTarget()));
+    levers.setToHideClickListener(() => stage.hide());
+});
