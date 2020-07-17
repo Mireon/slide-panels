4b8df5217e083d6a5f89623b70dc9aae0b297b58
Igor Kozhevnikov <igor.kozhevnikov@mireon.ru>
2020-07-17 16:45:43 +0300

    Issue-8 | CS.

--- src/backend/Widgets/Widget.php
+++ src/backend/Widgets/Widget.php
@@ -15,8 +15,8 @@ abstract class Widget implements WidgetInterface
 
     /**
      * The widget key.
-     * 
-     * @var string|null 
+     *
+     * @var string|null
      */
     private ?string $key = null;
 
